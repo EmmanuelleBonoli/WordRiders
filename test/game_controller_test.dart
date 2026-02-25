2d04eac064a46f538dcef079e9955b34a370039e
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2026-02-25 22:28:02 +0100

    fix TU

--- test/game_controller_test.dart
+++ test/game_controller_test.dart
@@ -1,9 +1,34 @@
+import 'dart:io';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:word_riders/features/gameplay/controllers/game_controller.dart';
 import 'package:word_riders/features/gameplay/services/word_service.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  
+  setUpAll(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+      const MethodChannel('plugins.flutter.io/path_provider'),
+      (MethodCall methodCall) async => Directory.systemTemp.path,
+    );
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+      const MethodChannel('xyz.luan/audioplayers.global'),
+      (MethodCall methodCall) async => 1,
+    );
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+      const MethodChannel('xyz.luan/audioplayers'),
+      (MethodCall methodCall) async => 1,
+    );
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMessageHandler(
+      'dev.flutter.pigeon.wakelock_plus_platform_interface.WakelockPlusApi.toggle',
+      (ByteData? message) async {
+        return const StandardMessageCodec().encodeMessage([null]);
+      },
+    );
+  });
+
   setUp(() {
     // Initialise les SharedPreferences virtuellement pour les tests
     SharedPreferences.setMockInitialValues({});
